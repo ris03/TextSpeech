074e8b9f4fcf0f95432f5e140462bacb2c6c1546
chanakyasethi96 <chanakya@cfeindia.com>
2018-07-03 10:59:09 +0530

    qq

--- src/pages/practice-test/practice-test.ts
+++ src/pages/practice-test/practice-test.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
+ 
 
 /**
  * Generated class for the PracticeTestPage page.
@@ -15,10 +16,34 @@ import { SpeechRecognition } from '@ionic-native/speech-recognition';
   selector: 'page-practice-test',
   templateUrl: 'practice-test.html',
 })
+
 export class PracticeTestPage {
   text:string='';
   constructor(public navCtrl: NavController, public navParams: NavParams,public tts: TextToSpeech,public speechRecognition: SpeechRecognition) {
   }
+
+// speechRs = speechRs || {};
+// speechRs.speechinit = function(lang,cb,bcolor,color,pitch,rate){
+//   this.speaker = new SpeechSynthesisUtterance();
+//   this.speaker.pitch=pitch || 1;
+//   this.speaker.rate=rate || 1;  
+//   this.lan = lang;
+//   var style = document.createElement('style');
+//  style.type = 'text/css';
+//  style.innerHTML = '.rsClass{background-color:'+(bcolor || "#4f91e6")+';color:'+(color || "#fff")+';}';
+//  document.getElementsByTagName('head')[0].appendChild(style);
+//   setTimeout(function(){
+//   speechRs.speaker.voice = speechSynthesis.getVoices().
+//     filter(function(voice) {  return voice.name == speechRs.lan; })[0];
+//   },500);
+//   if(lang == 'native'){
+//  cb(this);
+//   }else{
+//     setTimeout(function(){
+//   cb(speechRs)
+//   },1000);
+//   }
+//  }
   ionViewWillEnter() {
     this.speechRecognition.hasPermission()
       .then((hasPermission: boolean) => {
@@ -33,10 +58,10 @@ export class PracticeTestPage {
      });
     }
     speak(){
-      this.speechRecognition.startListening()
-      .subscribe(
-        (matches: Array<string>) => {
-          this.text = matches[0];})
-        }
-
-}
+      // this.speechRecognition.startListening()
+      // .subscribe(
+      //   (matches: Array<string>) => {
+      //     this.text = matches[0];})
+      //   }
+    }
+  }
